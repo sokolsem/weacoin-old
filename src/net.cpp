c504436bf23d183c7c7f867034f1dc24c878889f
Semen Sokolov <34645389+sokolsem@users.noreply.github.com>
2018-04-11 23:27:19 +0300

    Update seeds connection

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"walletbuilders.com", "node.walletbuilders.com"},
+    {"54.149.160.5", "54.149.160.5"},
     {"46.8.209.7", "46.8.209.7"},
 };
 
